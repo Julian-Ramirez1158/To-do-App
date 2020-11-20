1d1e98ff196f767910fcaad04f84fce500900ce9
Julian-Ramirez1158 <69184233+Julian-Ramirez1158@users.noreply.github.com>
2020-11-20 17:12:01 -0500

    Add form to input new items

---
+++ src/AddForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class AddTodo extends Component {
+    
+    render(){
+        return (
+            <div>
+                <form onSubmit={}>
+                    <label>Add new item:</label>
+                    <input type="text" onChange={} />
+                </form>
+            </div>
+        )
+    }
+}
